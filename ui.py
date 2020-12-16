a79c4006c1d676b6d62bdcd96a124020d126ef5d
spfave <sebpfave@gmail.com>
2020-12-16 18:56:15 -0500

    Base question card

--- ui.py
+++ ui.py
@@ -15,16 +15,23 @@ class QuizInterface(tk.Tk):
         super().__init__()
         self.title("Quizzler")
         self.config(padx=50, pady=50, background=THEME_COLOR)
+        self.init_UI()
         self.mainloop()
+
+    def init_UI(self):
+        self.q_card = QuestionCard(self)
+        self.q_card.grid(row=1, column=0, columnspan=2)
 
 
 # todo: create question card class
-class QuestionCard():
+class QuestionCard(tk.Canvas):
     """  """
 
-    def __init__(self):
+    def __init__(self, parent):
         """  """
-        pass
+        super().__init__(parent)
+        self.config(width=200, height=200, background="white")
+        self.question = self.create_text(100, 100, text="Test Question")
 
 
 # todo: create button class
