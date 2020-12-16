7134588c77bf69e9f0fa1bfd15c61dc0cf2b7132
spfave <sebpfave@gmail.com>
2020-12-15 21:26:20 -0500

    quiz GUI todos and class setup

--- ui.py
+++ ui.py
@@ -1,2 +1,31 @@
+
+# Constants
 THEME_COLOR = "#375362"
 
+
+# Classes
+# todo: create quiz interface class
+class QuizInterface(object):
+    """  """
+
+    def __init__(self):
+        """  """
+        pass
+
+
+# todo: create question card class
+class QuestionCard():
+    """  """
+
+    def __init__(self):
+        """  """
+        pass
+
+
+# todo: create button class
+class Button():
+    """  """
+
+    def __init__(self):
+        """  """
+        pass
