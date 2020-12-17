e73679c9866638cb4e60ab7fa8f67c15e3af99d8
spfave <sebpfave@gmail.com>
2020-12-16 21:45:26 -0500

    quiz_brain clean up

--- quiz_brain.py
+++ quiz_brain.py
@@ -19,10 +19,6 @@ class QuizBrain:
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
 
-        # user_answer = input(
-        #     f"Q.{self.question_number}: {q_text} (True/False): ")
-        # self.check_answer(user_answer)
-
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
