691badd360d1d89b9361add01971bdf234be5548
spfave <sebpfave@gmail.com>
2020-12-16 19:18:38 -0500

    ui updates

--- ui.py
+++ ui.py
@@ -3,6 +3,7 @@ import tkinter as tk
 
 # Constants
 THEME_COLOR = "#375362"
+q_font = ("Arial", 20)
 
 
 # Classes
@@ -14,18 +15,21 @@ class QuizInterface(tk.Tk):
         """  """
         super().__init__()
         self.title("Quizzler")
-        self.config(padx=25, pady=50, background=THEME_COLOR)
+        self.config(padx=20, pady=20, background=THEME_COLOR)
         self.init_UI()
         self.mainloop()
 
     def init_UI(self):
         self.score = Score(self)
         self.score.grid(row=0, column=1)
+
         self.q_card = QuestionCard(self)
         self.q_card.grid(row=1, column=0, columnspan=2)
+
         self.image_true = tk.PhotoImage(file="images/true.png")
         self.button_true = Button(self, image=self.image_true)
         self.button_true.grid(row=2, column=0)
+
         self.image_false = tk.PhotoImage(file="images/false.png")
         self.button_false = Button(self, image=self.image_false)
         self.button_false.grid(row=2, column=1)
@@ -37,7 +41,11 @@ class Score(tk.Label):
     def __init__(self, parent, *args, **kwargs):
         """  """
         super().__init__(parent, *args, **kwargs)
-        self.config(text=f"Score: 0")
+        self.config(
+            text=f"Score: 0",
+            background=THEME_COLOR,
+            foreground="white"
+        )
 
 
 class QuestionCard(tk.Canvas):
@@ -46,9 +54,9 @@ class QuestionCard(tk.Canvas):
     def __init__(self, parent, *args, **kwargs):
         """  """
         super().__init__(parent, *args, **kwargs)
-        self.config(width=200, height=200, background="white")
+        self.config(width=250, height=200, background="white")
         self.question = self.create_text(
-            100, 100, text="Test Question", width=180)
+            100, 100, text="Test Question", font=q_font, width=180)
 
 
 class Button(tk.Button):
@@ -57,4 +65,7 @@ class Button(tk.Button):
     def __init__(self, parent, *args, **kwargs):
         """  """
         super().__init__(parent, *args, **kwargs)
-        # self.config()
+        self.config(
+            relief="flat",
+            highlightthickness=0
+        )
