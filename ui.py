0f5bacac5e46dcb505d0422ec95eb674886e83cb
spfave <sebpfave@gmail.com>
2020-12-16 19:10:51 -0500

    base buttons

--- ui.py
+++ ui.py
@@ -23,32 +23,38 @@ class QuizInterface(tk.Tk):
         self.score.grid(row=0, column=1)
         self.q_card = QuestionCard(self)
         self.q_card.grid(row=1, column=0, columnspan=2)
+        self.image_true = tk.PhotoImage(file="images/true.png")
+        self.button_true = Button(self, image=self.image_true)
+        self.button_true.grid(row=2, column=0)
+        self.image_false = tk.PhotoImage(file="images/false.png")
+        self.button_false = Button(self, image=self.image_false)
+        self.button_false.grid(row=2, column=1)
 
 
 class Score(tk.Label):
     """  """
 
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
         """  """
-        super().__init__(parent)
+        super().__init__(parent, *args, **kwargs)
         self.config(text=f"Score: 0")
 
 
 class QuestionCard(tk.Canvas):
     """  """
 
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
         """  """
-        super().__init__(parent)
+        super().__init__(parent, *args, **kwargs)
         self.config(width=200, height=200, background="white")
         self.question = self.create_text(
             100, 100, text="Test Question", width=180)
 
 
-# todo: create button class
-class Button():
+class Button(tk.Button):
     """  """
 
-    def __init__(self):
+    def __init__(self, parent, *args, **kwargs):
         """  """
-        pass
+        super().__init__(parent, *args, **kwargs)
+        # self.config()
