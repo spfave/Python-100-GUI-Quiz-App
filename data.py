f8bac7f2105f9072c5cda66f4c9af83ea75a882a
spfave <sebpfave@gmail.com>
2020-12-16 21:44:41 -0500

    main and data refactoring

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import requests
+from question_model import Question
 
 
 # Functions
@@ -17,5 +18,18 @@ def generate_quiz_questions(num_questions=10, question_type="boolean"):
     return question_data
 
 
+def qdata_list_to_qobj_list(question_data):
+    question_bank = []
+
+    for question in question_data:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
+
+    return question_bank
+
+
 # Main
 question_data = generate_quiz_questions()
+question_bank = qdata_list_to_qobj_list(question_data)
